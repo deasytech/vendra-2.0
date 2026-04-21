1081cfe955934f33e81059d4e5a24221d305c17f
Desmond Awere <desmondawere@gmail.com>
2026-04-21 14:05:15 +0100

    fixed registration page issue

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -4,7 +4,7 @@
     <!-- Session Status -->
     <x-auth-session-status class="text-center" :status="session('status')" />
 
-    <form wire:submit.prevent="register" class="flex flex-col gap-6">
+    <form method="POST" wire:submit.prevent="register" class="flex flex-col gap-6">
         <!-- Name -->
         {{-- <flux:input wire:model="name" :label="__('Name')" type="text" required autofocus autocomplete="name"
             :placeholder="__('Full name')" /> --}}
